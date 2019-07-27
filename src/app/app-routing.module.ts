5e41d4fbb11c78d5adfd8c1aa521b1291fce40ce
Ganesh Chincholkar <33261835+ganeshpc@users.noreply.github.com>
2019-07-27 12:20:36 +0530

    Revert "Material navigation setup completed"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './components/home/home.component';
-import { CompetitionsComponent } from './components/competitions/competitions.component';
-import { DepartmentsComponent } from './components/departments/departments.component';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
-import { FaqComponent } from './components/faq/faq.component';
 
-
-const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'competitions', component: CompetitionsComponent},
-  {path: 'departments', component: DepartmentsComponent},
-  {path: 'contact-us', component: ContactUsComponent},
-  {path: 'about-us', component: AboutUsComponent},
-  {path: 'faq', component: FaqComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
