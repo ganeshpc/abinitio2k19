872a9f9096fb7ac7eb2dcd09ab523eee37556652
ganeshpc <gpc5464kh@gmail.com>
2019-07-28 22:35:10 +0530

    mongo db connection complete

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true }).then( () => {
+  console.log("Connected to the the database...");
+}).catch(() => {
+  console.log("Connection down!!!");
+});
 
 const competitionsRoutes = require('./routes/competitions');
 
