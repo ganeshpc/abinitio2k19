6e9f1fd19a0ee8a233caee16005a59d722866881
ganeshpc <gpc5464kh@gmail.com>
2019-07-28 08:36:55 +0530

    competition cards setup complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { MainNavComponent } from './navigation/main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,13 +11,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+
+import { AppComponent } from './app.component';
 import { CompetitionsComponent } from './components/competitions/competitions.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ContactUsComponent } from './components/contact-us/contact-us.component';
 import { DepartmentsComponent } from './components/departments/departments.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { HomeComponent } from './components/home/home.component';
-import { MatRippleModule } from '@angular/material/core';
+import { CompetitionCardComponent } from './components/competition-card/competition-card.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +32,8 @@ import { MatRippleModule } from '@angular/material/core';
     ContactUsComponent,
     DepartmentsComponent,
     FaqComponent,
-    HomeComponent
+    HomeComponent,
+    CompetitionCardComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +45,8 @@ import { MatRippleModule } from '@angular/material/core';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
-    MatRippleModule
+    MatRippleModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
