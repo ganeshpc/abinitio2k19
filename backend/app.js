4b16e4e9cbad3f20f09296e2dd06c57fa49989f6
ganeshpc <gpc5464kh@gmail.com>
2019-08-02 20:04:57 +0530

    comments added

--- backend/app.js
+++ backend/app.js
@@ -3,22 +3,29 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+
+//connecting mongo db
 mongoose.connect('mongodb://localhost/test?authSource=admin', { user: 'admin', pass: 'abinitio2k19', useNewUrlParser: true }).then( () => {
   console.log("Connected to the the database...");
 }).catch(() => {
   console.log("Connection down!!!");
 });
 
+//routing for competition route
 const competitionsRoutes = require('./routes/competitions');
 
+//initial get url
 app.get('/message', (req, res) => {
   console.log("hello");
   res.send("hello");
 });
 
+//defining schemas
 const Competition = require('./schemas/competition.schema');
 const Student = require('./schemas/student.schema');
 
+
+//instruct server to route the url of below given file in competitionRout
 app.use('/api/competitions', competitionsRoutes);
 
 module.exports = app;
