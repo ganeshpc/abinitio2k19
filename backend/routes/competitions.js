aa722f0e4f3f84572f97c6096a59f755bf0a6b20
ganeshpc <gpc5464kh@gmail.com>
2019-08-02 22:04:59 +0530

    post request for competition base setup

--- backend/routes/competitions.js
+++ backend/routes/competitions.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 router.get('/', (req, res,) => {
   res.send("competitions");
-})
+});
+
+router.post('/', (req, res) => {
+  res.send(req.body.message);
+});
 
 module.exports = router;
